57e0446645732f4d45c344a02091f2e6d24bd7f5
Luca <32082555+lukaneco@users.noreply.github.com>
2020-07-27 10:58:14 -0300

    gg

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,13 +1,49 @@
 fname="$1"
 sed 's/\s*,*\s*$//g' "$fname" > tmp.csv
 op=$(echo "$fname" | cut -d"." -f 1)
+TABLE=$op"-table"
+DATABASE=$op
 opfile="$op.sql"
 op="\`$op\`"
 columns=$(head --lines=1 tmp.csv | sed 's/,/`,`/g' | tr -d "\r\n")
+
+
 columns="\`$columns\`"
 tail --lines=+2 tmp.csv | while read l ; do
 values=$(echo $l | sed 's/,/\",\"/g' | tr -d "\r\n")
 values="\"$values\""
 echo "INSERT INTO $op($columns) VALUES ($values);"
 done > "$opfile"
+
+
+
+#CREATE TABLE Persons(PersonID int,LastName varchar(255));
+#$tablecolumnstypes="\`$tablecolumnstypes\`"
+#echo $tablecolumnstypes
+#https://stackoverflow.com/questions/9998596/create-mysql-table-directly-from-csv-file-using-the-csv-storage-engine
+#http://positon.org/import-csv-file-to-mysql
+
+
+DELIM=","
+
+#CSV="$1"
+CSV="tmp.csv"
+
+
+FIELDS=$(head -1 "$CSV" | sed -e 's/'$DELIM'/` varchar(255),\n`/g' -e 's/\r//g')
+echo $FIELDS
+FIELDS='`'"$FIELDS"'` varchar(255)'
+
+echo $FIELDS
+
+sed "1iCREATE DATABASE '$DATABASE';\nUSE '$DATABASE';" $opfile
+
+#sed "1iCREATE DATABASE '$DATABASE';\nUSE '$DATABASE';" $opfile
+sed "3iDROP TABLE IF EXISTS '$TABLE';\nCREATE TABLE '$TABLE' ($FIELDS)" $opfile
+#\nCREATE TABLE '$TABLE' ('$FIELDS');
+echo "DROP TABLE IF EXISTS '$TABLE';\nCREATE TABLE '$TABLE' ($FIELDS)"
+#echo "DROP TABLE IF EXISTS '$TABLE';"
+#echo $FIELDS
+#echo "CREATE TABLE '$TABLE' ($FIELDS);"
+
 rm tmp.csv
---
+++ scripts/my.sql
@@ -0,0 +1,6 @@
+INSERT INTO `my`(`Serial Number`,`Company Name`,`Employee Markme`,`Description`,`Leave`) VALUES ("9788189999599","TALES OF SHIVA","Mark","mark","0");
+INSERT INTO `my`(`Serial Number`,`Company Name`,`Employee Markme`,`Description`,`Leave`) VALUES ("9780099578079","1Q84 THE COMPLETE TRILOGY","HARUKI MURAKAMI","Mark","0");
+INSERT INTO `my`(`Serial Number`,`Company Name`,`Employee Markme`,`Description`,`Leave`) VALUES ("9780198082897","MY KUMAN","Mark","Mark","0");
+INSERT INTO `my`(`Serial Number`,`Company Name`,`Employee Markme`,`Description`,`Leave`) VALUES ("9780007880331","THE GOD OF SMAAL THINGS","ARUNDHATI ROY","4TH HARPER COLLINS","2");
+INSERT INTO `my`(`Serial Number`,`Company Name`,`Employee Markme`,`Description`,`Leave`) VALUES ("9780545060455","THE BLACK CIRCLE","Mark","4TH HARPER COLLINS","0");
+INSERT INTO `my`(`Serial Number`,`Company Name`,`Employee Markme`,`Description`,`Leave`) VALUES ("9788126525072","THE THREE LAWS OF PERFORMANCE","Mark","4TH HARPER COLLINS","0");
