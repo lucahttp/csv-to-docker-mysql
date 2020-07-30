b514f0506faf4b45d95f7f30573158e0b53fc1b7
Luca <32082555+lukaneco@users.noreply.github.com>
2020-07-29 21:09:46 -0300

    stable

--- exec.ps1
+++
@@ -1,3 +0,0 @@
-docker build --pull --rm -f "1.dockerfile" -t csvtodockermysql:latest "."
-docker run -i -t -d -p 3306:3306 --pid=host --name mysql_container csvtodockermysql:latest
-docker inspect --format '{{ .NetworkSettings.IPAddress }}' mysql_container
