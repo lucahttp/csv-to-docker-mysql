7ef18e2a51aa0f99cbde94df0c29fe4e1fcaf36d
Luca <32082555+lukaneco@users.noreply.github.com>
2020-08-05 17:59:09 -0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,3 +2,8 @@ $DOCKER_ACC="lukaneco"
 $DOCKER_REPO="csv2mysql"
 docker build -t lukaneco/csv2mysql:latest csv2docker.dockerfile
 docker push lukaneco/csv2mysql:latest
+
+
+source:
+    https://numidian.io/convert
+    http://convertcsv.com/csv-to-sql.htm
